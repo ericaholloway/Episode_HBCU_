e9be8539eb85566bb76ac6b47aa73d7095bd1276
erica <ericaholloway2019@gmail.com>
2019-07-30 16:56:50 -0400

    Another box
    Press C to continue convo

--- scene_2.py
+++ scene_2.py
@@ -8,3 +8,11 @@ def scene_2():
     #Penny
     img = loadImage ("penny1.PNG")
     image(img,1020,400,200,300)
+
+    #Click S to continue 
+    fill (240, 10, 63, 160)
+    rect(100, 650, 230, 50)
+    n= createFont ("Ubuntu-Medium.ttf",18)
+    textFont(n)
+    fill (0, 0, 0)
+    text("Click 'C' to continue the conversation",100, 680)
