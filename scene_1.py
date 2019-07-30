79d90e4b79c81d0f139372fe669f859e3921ad35
erica <ericaholloway2019@gmail.com>
2019-07-30 16:05:01 -0400

    add a box
    click "s" to go to class

--- scene_1.py
+++ scene_1.py
@@ -15,3 +15,11 @@ def scene_1():
     text("It's your first day at HBC University! You've just said your goodbyes to your family", 100, 50)
     text("and now you're on your own. Time to figure things out, and fast!", 100, 82)
     text("Your first class starts in 10  minutes, don't be late!", 100, 114)
+
+    fill (240, 10, 63, 160)
+    rect(100, 650, 230, 50)
+    
+    n= createFont ("Ubuntu-Medium.ttf",18)
+    textFont(n)
+    fill (0, 0, 0)
+    text("Click 'S' to continue to class",100, 680)
